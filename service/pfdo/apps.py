1ff635d1bbd216506b8674bf679320ac9e61ce23
Ванёк <105334936+HumanSuit7688@users.noreply.github.com>
2024-12-22 06:12:05 +1100

    Delete service/pfdo directory
    clear

--- service/pfdo/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PfdoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pfdo'
--- service/pfdo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('section/<int:section_id>', views.section, name='section'),
-]
