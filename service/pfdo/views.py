7afe605b9e714e4342a0b98e896bcebe766b90fb
Ivan <ivanu1706@gmail.com>
2024-12-21 19:58:27 +1100

    preview list of sections

--- service/pfdo/views.py
+++ service/pfdo/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from .func_db import get_section_from_id, get_search_names_sections
+from .func_db import get_section_from_id, get_search_names_sections, get_output_info_sections
 
 def index(request):
-    section = get_search_names_sections()
+    sections = get_output_info_sections()
 
+    html_list = f'<ul>'
 
+    for info in sections:
+        info_str = f'{info["id"]} | {info["short_name"]} | {info["address"]} | {info["age"]}'
+        html_list += f'<li>{info_str}</li>'
 
-    return render(request,'index.html', {'section' : section})
+    html_list += f'</ul>'
+
+    return render(request,'index.html', {'section' : html_list})
